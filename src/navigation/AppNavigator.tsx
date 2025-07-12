3f79436328318de33594a989ddcff6197b7fa686
Abdalrahman Aboujeeb <abdalrahmanaboujeeb@Abdalrahmans-MacBook-Air.local>
2025-07-12 21:47:19 +0800

    feat: keep user logged in if token didn't expire

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import HomeScreen from '../screens/HomeScreen';
+import { useAuth } from '../context/AuthContext';
 
 export type RootStackParamList = {
     Login: undefined;
@@ -14,11 +14,18 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
+    const { user } = useAuth();
+
     return (
-        <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Navigator>
+            {user ? (
+                <Stack.Screen name="Home" component={HomeScreen} />
+            ) : (
+                <>
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Signup" component={SignupScreen} />
+                </>
+            )}
         </Stack.Navigator>
     );
 };
